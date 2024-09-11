e4578fc8c33bf9b7277be9881e23af3785b20122
tungmath <nguyendinhtung993@gmail.com>
2024-09-11 16:36:57 +0700

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -10,14 +10,19 @@ myImage.onclick = () => {
     myImage.setAttribute("src", "image/123.jpg");
   } else {
     myImage.setAttribute("src", "image/pexels-marc-schulte-2779948.jpg");
+    myIamge.setAttribute("alt", "penguins");
   }
 }
 
 
 function setUserName(){
     const myName = prompt("Please enter your name.");
-    localStorage.setItem("name", myName);
-    myHeading.textContent = `Mozilla rất ngầu\n Chào \"${myName}\", bạn khỏe ha!`;
+    if (!myName){
+      setUserName();
+    }else{
+      localStorage.setItem("name", myName);
+      myHeading.textContent = `Mozilla rất ngầu\n Chào \"${myName}\", bạn khỏe ha!`;
+    }
 };
 
 if (!localStorage.getItem("name")) {
